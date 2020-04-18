d81c1265ff9e827fea31777468fad820ad879002
vd89 <me@daxcodes.in>
2020-04-18 05:52:28 +0530

    the missingLetter

---
+++ missingLetter.js
@@ -0,0 +1,18 @@
+/*
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+*/
+
+function fearNotLetter(str) {
+    for (let i = 1; i < str.length; ++i){
+        if (str.charCodeAt(i)-str.charCodeAt(i-1) > 1) {
+            return String.fromCharCode(str.charCodeAt(i-1) +1)
+        }
+    }
+
+}
+
+console.log(fearNotLetter('abce'));
+console.log(fearNotLetter('abcdefghjkmn'));
+console.log(fearNotLetter('stuvxy'));
